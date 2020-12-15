db8d0a5d8ca5b34b1ef1a6aa083717f85bb64ae7
licyrus112 <licyrus4@gmail.com>
2020-12-16 03:03:19 +0800

    mainnn

--- php/AddExam.php
+++ php/AddExam.php
@@ -5,36 +5,44 @@
     $dbname = "lib";
 
     $ExamID = $_POST['ExamID'];
-    $QuestionType = $_POST['QuestionType'];
+    $QuestionID = $_POST['QuestionID'];
+
+    $ExamDate = $_POST['ExamDate'];
+    $StartTime = $_POST['StartTime'];
+    $EndTime = $_POST['EndTime'];
     $Question = $_POST['Question'];
 
-    $choiceA = $_POST['choiceA'];
-    $choiceB = $_POST['choiceB'];
-    $choiceC = $_POST['choiceC'];
-    $choiceD = $_POST['choiceD'];
+
+    $choiceA = @$_POST['choiceA'];
+    $choiceB = @$_POST['choiceB'];
+    $choiceC = @$_POST['choiceC'];
+    $choiceD = @$_POST['choiceD'];
 
     $Answer = $_POST['Answer'];
     $Score = $_POST['Score'];
 
     $connect = mysqli_connect($hostname, $username, $password, $dbname);
-    $query = "INSERT INTO `question`(`ExamID`, `QuestionType`, `Question`, `choiceA`, `choiceB`, `choiceC`, `choiceD`, `Answer`, `Score`) 
-    VALUES ('$ExamID', '$QuestionType', '$Question', '$choiceA', '$choiceB', '$choiceC', '$choiceD', '$Answer', '$Score')";
+    $query = "INSERT INTO `question`( `ExamDate`, `StartTime`, `EndTime`, `ExamID`, `QuestionID`, `Question`, `choiceA`, `choiceB`, `choiceC`, `choiceD`, `Answer`, `Score`) 
+                VALUES ('$ExamDate', '$StartTime', '$EndTime', '$ExamID', '$QuestionID', '$Question' , '$choiceA',  '$choiceB', '$choiceC', '$choiceD', '$Answer', '$Score')";
 
 
 
     $result = mysqli_query($connect, $query);
     if($result)
     {
+        return TRUE;
         echo 'Data Inserted';
     }
     
     else{
+        return FALSE;
         echo 'Data Not Inserted';
     }
-    
+
     mysqli_free_result($result);
     mysqli_close($connect);
 
 
 
 ?>
+
