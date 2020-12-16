5fceb4bae555afdf40a44f3268eed51b0112241c
t6703tcs <t6703tcs@gmail.com>
2020-12-16 23:58:19 +0800

    main

--- php/teacher.php
+++ php/teacher.php
@@ -97,17 +97,10 @@
                 <hr class="d-sm-none">
             </div>
             <div class="col-sm-8">
-                <h2>TITLE HEADING</h2>
-                <h5>Title description, Dec 7, 2017</h5>
-                <div class="fakeimg">Fake Image</div>
-                <p>Some text..</p>
-                <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                <br>
-                <h2>TITLE HEADING</h2>
-                <h5>Title description, Sep 2, 2017</h5>
-                <div class="fakeimg">Fake Image</div>
-                <p>Some text..</p>
-                <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                <h2>Please select function on your left.</h2>
+                <h5>Your User ID is: <?php echo htmlspecialchars($_COOKIE["userID"]);?></h5>
+                <p>You can click 'Create Exam' to schedule and add questions for an exam. <br>You can also click 'View Exam Result' to evaluates and views students' submitted answers. <br>If you want to reset password, you can click 'Change Password'! </p>
+
             </div>
         </div>
     </div>
